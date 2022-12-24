4ab2a3f0a2fbf1fcbbfc13f78aa6cb55048e7dd3
frank <frank@Debian.myguest.virtualbox.org>
2022-12-24 14:03:58 +0100

    add method subscribe to CodesysAdapter

--- src/component/CodesysAdapter.cpp
+++ src/component/CodesysAdapter.cpp
@@ -1,11 +1,44 @@
 #include "CodesysAdapter.h"
 
-CodesysAdapter::CodesysAdapter( std::string const &_type, std::string const &_name ) : type( _type ), name( _name )
+#include "copa-pdk/component/ComponentController.h"
+
+CodesysAdapter::CodesysAdapter( std::string const &_type, std::string const &_name )
+    : CodesysAdapter( _type, _name, std::make_shared< COPA::ComponentController >() )
+{
+}
+
+CodesysAdapter::CodesysAdapter( std::string const &_type, std::string const &_name, std::shared_ptr< COPA::ComponentControllerIf > _componentController )
+    : type( _type ), name( _name ), componentController( _componentController )
 {
 }
 
 CodesysAdapter::~CodesysAdapter()
 {
+}
+
+
+void CodesysAdapter::subscribe()
+{
+    auto const runtimeAdapterTmp = componentController->get( "RuntimeAdapter", "Mickey Mouse" );
+    auto const selfCodesysAdapterTmp = componentController->get( "CodesysAdapter", "Goofy" );
+
+    auto const runtimeAdapter = std::reinterpret_pointer_cast< RuntimeAdapterIf >( runtimeAdapterTmp );
+    auto const myself = std::reinterpret_pointer_cast< RuntimeIf >( selfCodesysAdapterTmp );
+
+    runtimeAdapter->subscribe(name, myself);
+}
+
+void CodesysAdapter::startApplications()
+{
+}
+
+void CodesysAdapter::stopApplications()
+{
+}
+
+std::string CodesysAdapter::getRuntimeVersion()
+{
+    return std::string( "3.5.18.0" );
 }
 
 std::string CodesysAdapter::getType()
@@ -21,17 +54,4 @@ std::string CodesysAdapter::getName()
 std::string CodesysAdapter::getVersion()
 {
     return std::string( "0.0.0.1" );
-}
-
-void CodesysAdapter::startApplications()
-{
-}
-
-void CodesysAdapter::stopApplications()
-{
-}
-
-std::string CodesysAdapter::getRuntimeVersion()
-{
-    return std::string( "3.5.18.0" );
 }
--- src/component/CodesysAdapter.h
+++ src/component/CodesysAdapter.h
@@ -1,22 +1,30 @@
 #pragma once
 
+#include <memory>
+
+#include "copa-pdk/component/ComponentControllerIf.h"
+#include "runtime-sdk/RuntimeAdapterIf.h"
 #include "runtime-sdk/RuntimeIf.h"
 
 class CodesysAdapter : public RuntimeIf
 {
    public:
     CodesysAdapter( std::string const &_type, std::string const &_name );
+    CodesysAdapter( std::string const &_type, std::string const &_name, std::shared_ptr< COPA::ComponentControllerIf > _componentController );
     ~CodesysAdapter();
+
+    virtual void subscribe() override;
+    virtual void startApplications() override;
+    virtual void stopApplications() override;
+    virtual std::string getRuntimeVersion() override;
 
     virtual std::string getType() override;
     virtual std::string getName() override;
     virtual std::string getVersion() override;
 
-    virtual void startApplications() override;
-    virtual void stopApplications() override;
-    virtual std::string getRuntimeVersion() override;
-
    private:
     std::string type;
     std::string name;
+
+    std::shared_ptr< COPA::ComponentControllerIf > componentController;
 };
